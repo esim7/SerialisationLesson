8911253061b206c5ca22aa2500f10ff42b0e50b4
makishev@yahoo.com <makishev@yahoo.com>
2019-09-27 21:40:24 +0600

    class work

---
+++ SerialisationLesson/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace SerialisationLesson
+{
+    //[Serializable]
+    public class Book
+    {
+        //[NonSerialized]
+        //private Guid id = Guid.NewGuid();
+        //public Guid Id
+        //{
+        //    get 
+        //    {
+        //        return id;
+        //    }
+        //    set 
+        //    {
+        //        id = value;
+        //    }
+
+        //}
+        [XmlIgnore]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [XmlElement("FIO")]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ SerialisationLesson/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+
+
+namespace SerialisationLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book
+                {
+                    Name = "Poema",
+                    Author = "Pushkin",
+                    Price = 1000
+                },
+                new Book
+                {
+                    Name = "Stihi",
+                    Author = "tolstoy",
+                    Price = 1200
+                },
+
+            };
+            //
+            //var serializer = new XmlSerializer(typeof(List<Book>));//books.gettype
+
+            //using (var stream = File.CreateText("1.xml"))
+            //{
+            //    serializer.Serialize(stream, books);
+            //}
+
+            //using (var stream = File.OpenRead("1.xml"))
+            //{
+            //    var result = serializer.Deserialize(stream) as List<Book>;
+            //}
+
+
+            //сериализация c помощью библиотеки Newtonsoft.Json;
+            string json = JsonConvert.SerializeObject(books);
+
+            var result = JsonConvert.DeserializeObject<List<Book>>(json);
+
+        }
+    }
+}
